ed6bcf062c14f76713b961dfae1a42d698242cec
Dennis Ozturk <dennis.dada@hotmail.se>
2017-12-02 01:38:14 +0100

    Done project

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var expressValidator = require('express-validator');
+var mongojs = require('mongojs');
+var db = mongojs('customapp', ['users']);
+var ObjectId = mongojs.ObjectId;
 
 var app = express();
 /*
@@ -23,28 +27,75 @@ app.use(bodyParser.urlencoded({extended: false}));
 // Set Static Path
 app.use(express.static(path.join(__dirname, 'public')));
 
-var users = [
-  {
-    first_name: 'John',
-    last_name: 'doe',
-    email: 'johndoe@hotmail.com'
+// Global Vars
+app.use(function(req, res, next){
+  res.locals.errors = null;
+  next();
+});
+
+//Express Validator Middleware
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value){
+    var namespace = param.split('.'),
+    root = namespace.shift(),
+    formParam = root;
+
+  while(namespace.length){
+    formParam += '[' + namespace.shift() + ']';
   }
-  {
-    first_name: 'Dennis',
-    last_name: 'Ozturk',
-    email: 'johndoe@hotmail.com'
+  return{
+    param: formParam,
+    msg: msg,
+    value: value
+  };
   }
-  {
-    first_name: 'Jill',
-    last_name: 'Jackson',
-    email: 'johndoe@hotmail.com'
-  }
-]
+}));
 
 app.get('/', function(req, res){
-  res.render('index', {
-    title: 'Customers'
-  });
+  db.users.find(function(err, docs){
+    res.render('index', {
+      title: 'Customers',
+      users: docs
+    });
+  })
+});
+
+app.post('/users/add', function(req, res){
+
+  req.checkBody('first_name', 'First Name is Required').notEmpty();
+  req.checkBody('last_name', 'Last Name is Required').notEmpty();
+  req.checkBody('email', 'Email is Required').notEmpty();
+
+  var  errors = req.validationErrors();
+
+  if(errors){
+    res.render('index', {
+      title: 'Customers',
+      users: users,
+      errors: errors
+    });
+  }else {
+    var newUser = {
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email
+    }
+    db.users.insert(newUser, function(err, result){
+      if(err){
+          console.log(err);
+      }
+        res.redirect('/');
+    });
+  }
+});
+
+app.delete('/users/delete/:id', function(req, res){
+  db.users.remove({_id: ObjectId(req.params.id)}, function(err, result){
+    if(err){
+      console.log(err);
+    }
+    res.redirect('/');
+  })
 });
 
 app.listen(3000, function(){
